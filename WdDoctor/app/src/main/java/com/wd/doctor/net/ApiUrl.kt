42c5efc8fe9a5781e1e1ee7c871b70f043988f96
huanyingyuequezuijihe <1165413404@qq.com>
2020-05-18 20:58:27 +0800

    05-18:路径

--- WdDoctor/app/src/main/java/com/wd/doctor/net/ApiUrl.kt
+++ WdDoctor/app/src/main/java/com/wd/doctor/net/ApiUrl.kt
@@ -4,9 +4,27 @@ package com.wd.doctor.net
  * @author 王阳
  * Class :1708A
  * @description:
- * @date :2020/5/18 19:50
+ * @date :2020/5/18 20:48
  * @classname :ApiUrl
- * 路径
  */
-class ApiUrl {
+class ApiUrl private constructor(){
+    //静态
+    companion object{
+        val apiUrl by lazy {ApiUrl()}
+    }
+    //发送给验证码
+    fun sendEmailCode():String{
+        val url:String="http://mobile.bwstudent.com/health/doctor/v1/sendEmailCode"
+        return url
+    }
+    //申请入驻
+    fun applyJoin():String{
+        val url:String="http://mobile.bwstudent.com/health/doctor/v1/applyJoin"
+        return url
+    }
+    //登录
+    fun login():String{
+        val url:String="http://mobile.bwstudent.com/health/doctor/v1/login"
+        return url
+    }
 }
