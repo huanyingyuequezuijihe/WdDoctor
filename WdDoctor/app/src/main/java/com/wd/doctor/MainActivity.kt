c0e1d9b67dae2b97fcf7ff558f2625313117b61a
huanyingyuequezuijihe <1165413404@qq.com>
2020-05-18 17:28:48 +0800

    05-18:适配

--- WdDoctor/app/src/main/java/com/wd/doctor/MainActivity.kt
+++ WdDoctor/app/src/main/java/com/wd/doctor/MainActivity.kt
@@ -2,11 +2,14 @@ package com.wd.doctor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wd.doctor.util.RsaCoder
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val encryptByPublicKey = RsaCoder.encryptByPublicKey("yang.1998.")
+        println("==========="+encryptByPublicKey)
     }
 }
