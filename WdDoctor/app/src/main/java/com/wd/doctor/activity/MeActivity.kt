1265f8b726f560739fa86cba54e4f0ebc377ed65
huanyingyuequezuijihe <1165413404@qq.com>
2020-05-19 19:36:41 +0800

    05-19:首页布局，网络工具类

--- WdDoctor/app/src/main/java/com/wd/doctor/activity/MeActivity.kt
+++ WdDoctor/app/src/main/java/com/wd/doctor/activity/MeActivity.kt
@@ -5,14 +5,8 @@ import android.os.Bundle
 import com.wd.doctor.R
 import com.wd.doctor.base.BaseActivity
 
-class HomeActivity : BaseActivity(){
+class MeActivity :BaseActivity() {
     override fun initLayoutId(): Int {
-        return R.layout.activity_home
-    }
-    override fun initListener() {
-        super.initListener()
-    }
-    override fun initData() {
-        super.initData()
+        return R.layout.activity_me
     }
 }
