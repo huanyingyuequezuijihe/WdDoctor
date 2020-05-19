afee04b1a0fc9ce18b14828b834cfcb48565bb68
huanyingyuequezuijihe <1165413404@qq.com>
2020-05-19 15:14:32 +0800

    05-19:ApiUrl

--- WdDoctor/app/src/main/java/com/wd/doctor/net/ApiUrl.kt
+++ WdDoctor/app/src/main/java/com/wd/doctor/net/ApiUrl.kt
@@ -7,25 +7,10 @@ package com.wd.doctor.net
  * @date :2020/5/18 20:48
  * @classname :ApiUrl
  */
-class ApiUrl private constructor(){
-    //静态
-    companion object{
-        val BASE_URL="http://mobile.bwstudent.com/"
-        val apiUrl by lazy {ApiUrl()}
-    }
+object ApiUrl{
+    const val BASE_URL="http://mobile.bwstudent.com/"
     //发送给验证码
-    fun sendEmailCode():String{
-        val url:String="http://mobile.bwstudent.com/health/doctor/v1/sendEmailCode"
-        return url
-    }
-    //申请入驻
-    fun applyJoin():String{
-        val url:String="http://mobile.bwstudent.com/health/doctor/v1/applyJoin"
-        return url
-    }
-    //登录
-    fun login():String{
-        val url:String="http://mobile.bwstudent.com/health/doctor/v1/login"
-        return url
-    }
+    const val sendEmailCode="health/doctor/v1/sendEmailCode"
+    const val applyJoin="health/doctor/v1/applyJoin"
+    const val login="health/doctor/v1/login"
 }
